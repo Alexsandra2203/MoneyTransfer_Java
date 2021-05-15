50d05f2afbff7b2fe01f1c680b851ed8e7a86aec
Aleksandra Kuptsaeva <mr.february079@gmail.com>
2021-05-15 09:45:57 +0300

    main

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+    int current = 2_000_000_000; // текущий баланс
+    int transfer = 500_000_000; // перевод
+    int total = current + transfer; // итог
+    System.out.println(total);
+}
+}
